37ed70081103690fccbd581c16579e962cf7ce76
Ruan Scherer <ruan.vscherer@gmail.com>
2020-11-10 10:54:54 -0300

    Create seller entity

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,11 +1,23 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Application {
 
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
         System.out.println(department);
+
+        Seller seller = new Seller(
+                1,
+                "Ruan",
+                "ruan.vscherer@gmail.com",
+                new Date(),
+                2800.00, department
+        );
+        System.out.println(seller);
     }
 }
